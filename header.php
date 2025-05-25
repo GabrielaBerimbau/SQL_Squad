4206c332f55cf6c1244d10e71d54979cd1903118
Alessandro Paravano <u24713122@tuks.co.za>
2025-05-25 14:57:55 +0200

    fixed header

--- header.php
+++ header.php
@@ -2,6 +2,12 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
+
+if(!isset($_SESSION['user_id'])){
+    header('Location: login.php');
+    exit();
+}
+
 ?>
 <nav class="navbar">
     <ul>
